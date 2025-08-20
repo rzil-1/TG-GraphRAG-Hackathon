925e47e36536e57567ba3b2f6bd279f32a0002b4
Chengbiao Jin <chengbiao.jin@tigergraph.com>
2025-08-20 15:23:26 -0700

    revise markdown chunker for size control

--- common/chunkers/markdown_chunker.py
+++ common/chunkers/markdown_chunker.py
@@ -13,24 +13,26 @@
 # limitations under the License.
 
 from common.chunkers.base_chunker import BaseChunker
-from langchain_text_splitters.markdown import ExperimentalMarkdownSyntaxTextSplitter
+from langchain_text_splitters.markdown import ExperimentalMarkdownSyntaxTextSplitter, MarkdownTextSplitter
 
 class MarkdownChunker(BaseChunker):
     
     def __init__(
         self,
-        chunk_size: int = 1024,
-        chunk_overlap: int = 0
+        chunk_size: int = 4192,
+        chunk_overlap: int = 256
     ):
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
     def chunk(self, input_string):
-        md_splitter = ExperimentalMarkdownSyntaxTextSplitter()
+        #md_splitter = ExperimentalMarkdownSyntaxTextSplitter()
+        md_splitter = MarkdownTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
 
         md_chunks = md_splitter.split_text(input_string)
 
-        return [x.page_content for x in md_chunks]
+        #return [x.page_content for x in md_chunks]
+        return md_chunks
 
     def __call__(self, input_string):
         return self.chunk(input_string)
